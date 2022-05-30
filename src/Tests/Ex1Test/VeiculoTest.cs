bccbc8ccfeeb87e499c33cff3d758576cf6f5b3a
aozi <aozi@gft.com>
2022-05-30 15:12:12 -0300

    test: Add test abastecer from Ex1Veiculo

--- src/Tests/Ex1Test/VeiculoTest.cs
+++ src/Tests/Ex1Test/VeiculoTest.cs
@@ -7,7 +7,7 @@ namespace Ex1Test
     public class VeiculoTest
     {
         [Fact]
-        public void Acelerar_RetornaVelocidadeMaiorQueZero()
+        public void Acelerar_ChamaMetodo_RetornaVelocidadeMaiorQueZero()
         {
             //Arrange
             var testVeiculo = new Veiculo("Chevrolet", "Prisma", "EJE1839", "Vermelho", 150000, 18000);
@@ -17,6 +17,38 @@ namespace Ex1Test
             testVeiculo.acelerar();
             //Assert
             Assert.Equal(expected, testVeiculo.Velocidade);
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData(40)]
+        [InlineData(60)]
+
+        public void Abastecer_RecebeNumeroInteiro_RetornaQuantidadeDeCombutivel(int QtdCombustivel)
+        {
+            //Arrange
+            var testVeiculo = new Veiculo("Chevrolet", "Prisma", "EJE1839", "Vermelho", 150000, 18000);
+            var expected = QtdCombustivel;
+            //Act
+            testVeiculo.abastecer(QtdCombustivel);
+            //Assert
+            Assert.Equal(expected, testVeiculo.litrosCombustivel);
+        }
+
+        [Theory]
+        [InlineData(70)]
+        [InlineData(62)]
+        [InlineData(85)]
+
+        public void Abastecer_RecebeNumeroInteiroMaiorQue60_RetornaQuantidadeDeCombutivelIgualZero(int QtdCombustivel)
+        {
+            //Arrange
+            var testVeiculo = new Veiculo("Chevrolet", "Prisma", "EJE1839", "Vermelho", 150000, 18000);
+            var expected = 0;
+            //Act
+            testVeiculo.abastecer(QtdCombustivel);
+            //Assert
+            Assert.Equal(expected, testVeiculo.litrosCombustivel);
         }
     }
 }
