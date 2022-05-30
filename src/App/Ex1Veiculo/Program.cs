0f54fa67019ad3eeda6d73314e0a4a50f72d3306
aozi <aozi@gft.com>
2022-05-30 15:36:28 -0300

    feat: Add new app Ex1Veiculo

---
+++ src/App/Ex1Veiculo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Ex1Veiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao teste drive do seu veículo!");
+            Console.WriteLine("Antes de começar vamos fazer o cadastro do seu veículo:");
+
+            Console.WriteLine("Digite a marca do seu veículo:");
+            string? marca = Console.ReadLine();
+
+            Console.WriteLine("Digite o modelo do seu veículo:");
+            string? modelo = Console.ReadLine();
+
+            Console.WriteLine("Digite a placa do seu veículo:");
+            string? placa = Console.ReadLine();
+
+            Console.WriteLine("Digite a cor do seu veículo:");
+            string? cor = Console.ReadLine();
+
+            Console.WriteLine("Digite a quilometragem do seu veículo:");
+            string? km = Console.ReadLine();
+            float convertKm = float.Parse(km);
+
+            Console.WriteLine("Digite o preço do seu veículo:");
+            string? preco = Console.ReadLine();
+            double convertPreco = double.Parse(preco);
+
+            Veiculo testDrive = new Veiculo(marca, modelo, placa, cor, convertKm, convertPreco);
+
+            Console.WriteLine("Agora vamos iniciar o teste do seu veículo");
+            string app = "2";
+
+            while (app == "2")
+            {
+                Console.WriteLine("Escolha o que deseja fazer informando um dos números abaixo");
+                Console.WriteLine("1 - Acelerar;\r\n2 - Abastecer;\r\n3 - Frear;\r\n4 - Pintar;\r\n5 - Ligar;\r\n6 - Desligar");
+                string escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        testDrive.acelerar();
+                        break;
+                    case "2":
+                        Console.WriteLine("Digite a quantidade de combustível em litros:");
+                        string qtdCombustivel = Console.ReadLine();
+                        int convertQtdCombustivel = int.Parse(qtdCombustivel);
+                        testDrive.abastecer(convertQtdCombustivel);
+                        break;
+                    case "3":
+                        testDrive.frear();
+                        break;
+                    case "4":
+                        Console.WriteLine("Digite nova cor do veículo");
+                        string corVeiculo = Console.ReadLine();
+                        testDrive.pintar(corVeiculo);
+                        break;
+                    case "5":
+                        testDrive.ligar();
+                        break;
+                    case "6":
+                        testDrive.desligar();
+                        if (testDrive.isLigado == false)
+                        {
+                            Console.WriteLine("Você deseja sair do teste drive?\r\n1 - Sim\r\n2 - Não");
+                            app = Console.ReadLine();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/App/Ex1Veiculo/Veiculo.cs
@@ -0,0 +1,121 @@
+using System;
+
+namespace Ex1Veiculo
+{
+    public class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public float Km { get; set; }
+        public bool isLigado = false;
+        public int litrosCombustivel = 0;
+        public int Velocidade = 0;
+        public double Preco;
+
+        public Veiculo(string marca, string modelo, string placa, string cor, float km, double preco)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;
+            Cor = cor;
+            Km = km;
+            Preco = preco;
+        }
+        public int acelerar()
+        {
+            if (isLigado == true)
+            {
+                Velocidade = Velocidade + 20;
+                Console.WriteLine("O veículo aumentou sua velocidade!");
+                Console.WriteLine("Velocidade atual: " + Velocidade);
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Seu veículo está desligado, ligue ele antes de acelerar");
+                return 0;
+            }
+
+        }
+
+        public int abastecer(int combustivel)
+        {
+            if (litrosCombustivel + combustivel > 60)
+            {
+                int qtdPossivel = 60 - litrosCombustivel;
+                Console.WriteLine("A Quantidade de combustível excede o total do tanque.");
+                Console.WriteLine("Quantidade de combustível para encher o tanque: " + qtdPossivel + " litros");
+                return 0;
+            }
+            else
+            {
+                litrosCombustivel = litrosCombustivel + combustivel;
+                Console.WriteLine("Veículo abastecido com sucesso");
+                Console.WriteLine("Quantidade de combustível: " + litrosCombustivel + " litros");
+                return 1;
+            }
+        }
+
+        public int frear()
+        {
+            if (Velocidade == 0)
+            {
+                Console.WriteLine("O veículo está parado");
+                return 0;
+            }
+            else
+            {
+                Velocidade = Velocidade - 20;
+                Console.WriteLine("Velocidade diminuida para " + Velocidade);
+                return 1;
+            }
+        }
+
+        public void pintar(string cor)
+        {
+            Cor = cor;
+            Console.WriteLine("O veículo agora está pintado na cor " + cor);
+        }
+
+        public int ligar()
+        {
+            if (isLigado == false)
+            {
+                isLigado = true;
+                Console.WriteLine("O veículo foi ligado");
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("O veículo já está ligado");
+                return 0;
+            }
+        }
+
+        public int desligar()
+        {
+            if (isLigado == true)
+            {
+                if (Velocidade > 0)
+                {
+                    Console.WriteLine("Veículo em movimento, pare ele antes de desligar");
+                    return 0;
+                }
+                else
+                {
+                    isLigado = false;
+                    Console.WriteLine("O veículo foi desligado");
+                    return 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("O veículo já está desligado");
+                return 0;
+            }
+        }
+
+    }
+}
