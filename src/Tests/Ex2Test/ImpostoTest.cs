0fdc796f750eb1b1a3cce1c61acd813236adcdf9
aozi <aozi@gft.com>
2022-05-31 10:33:10 -0300

    test: Adds tests to the Ex2Imposto VideoGame and Loja class

--- src/Tests/Ex2Test/ImpostoTest.cs
+++ src/Tests/Ex2Test/ImpostoTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Ex2Imposto.Model;
+using System.Collections.Generic;
 
 namespace Ex2Test
 {
@@ -30,5 +31,98 @@ namespace Ex2Test
             Assert.Equal(expected, result);
         }
 
+        [Fact]
+        public void CalculaImposto_ChamaMetodoVideoGameUsado_RetornaValorImposto()
+        {
+            //Arrange
+            var VideoGame = new VideoGame("PS4", 1800, 100, "Sony", "Slim", true);
+            //Act
+            var result = VideoGame.calculaImposto();
+            var expected = 1800 * 0.25;
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CalculaImposto_ChamaMetodoVideoGameNovo_RetornaValorImposto()
+        {
+            //Arrange
+            var VideoGame = new VideoGame("PS4", 1800, 100, "Sony", "Slim", false);
+            var expected = 1800 * 0.45;
+            //Act
+            var result = VideoGame.calculaImposto();            
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ListaLivros_VerificaSeEstaVazia_RetornaTrue()
+        {
+            //Arrange
+            List<Livro> livros = new List<Livro>();
+            List<VideoGame> games = new List<VideoGame>();
+            var loja = new Loja("Americanas", "12345678", livros, games);
+            //Act
+            //Assert
+            Assert.Empty(loja.Livros);
+        }
+
+        [Fact]
+        public void ListaLivros_VerificaSePossueLivros_RetornaTrue()
+        {
+            //Arrange
+            Livro livro1 = new Livro("Harry Potter", 40, 50, "J. K. Rowling", "fantasia", 300);
+            List<Livro> livros = new List<Livro>();
+            livros.Add(livro1);
+            List<VideoGame> games = new List<VideoGame>();
+            var loja = new Loja("Americanas", "12345678", livros, games);
+            //Act
+            //Assert
+            Assert.NotEmpty(loja.Livros);
+        }
+
+        [Fact]
+        public void ListaVideoGames_VerificaSeEstaVazia_RetornaTrue()
+        {
+            //Arrange
+            List<Livro> livros = new List<Livro>();
+            List<VideoGame> games = new List<VideoGame>();
+            var loja = new Loja("Americanas", "12345678", livros, games);
+            //Act
+            //Assert
+            Assert.Empty(loja.VideoGames);
+        }
+
+        [Fact]
+        public void ListaVideoGames_VerificaSePossueVideoGames_RetornaTrue()
+        {
+            //Arrange
+            VideoGame videoGame1 = new VideoGame("PS4", 1800, 100, "Sony", "Slim", false);
+            List<Livro> livros = new List<Livro>();
+            List<VideoGame> games = new List<VideoGame>();
+            games.Add(videoGame1);
+            var loja = new Loja("Americanas", "12345678", livros, games);
+            //Act
+            //Assert
+            Assert.NotEmpty(loja.VideoGames);
+        }
+
+        [Fact]
+        public void CalculaPatrimonio_SomaValoresDeLivrosEVideoGames_RetornaValorTotalDoPatrimonio()
+        {
+            //Arrange
+            VideoGame videoGame1 = new VideoGame("PS4", 1800, 100, "Sony", "Slim", false);
+            Livro livro1 = new Livro("Harry Potter", 40, 50, "J. K. Rowling", "fantasia", 300);
+            List<Livro> livros = new List<Livro>();
+            List<VideoGame> games = new List<VideoGame>();
+            games.Add(videoGame1);
+            livros.Add(livro1);
+            var loja = new Loja("Americanas", "12345678", livros, games);
+            var expected = 1800 + 40;
+            //Act
+            var result = loja.calculaPatrimonio();
+            //Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
