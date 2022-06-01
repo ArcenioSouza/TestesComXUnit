753da7f739b0e1a1c002af762856bde8bbfed08b
aozi <aozi@gft.com>
2022-06-01 09:46:17 -0300

    fix: Fix bin folder bug

--- src/App/Main/Program.cs
+++ src/App/Main/Program.cs
@@ -2,18 +2,21 @@
 using Ex1Veiculo;
 using Ex2Imposto;
 using Ex3Personagem;
+using Ex7Funcionarios;
+using Jokenpo;
 
 namespace Main
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("Bem vindo ao App Colection Exercises");
             Console.WriteLine("------------------------------------");
             Console.WriteLine($"Escolha uma das opções de exercicios para visualiza-lo:");
             Console.WriteLine("------------------------------------");
-            Console.WriteLine("1 - Exercicio sobre Veiculo;\r\n2 - Exercicio sobre Imposto;\r\n3 - Exercicio sobre Personagem");
+            Console.WriteLine("1 - Exercicio sobre Veiculo;\r\n2 - Exercicio sobre Imposto;\r\n3 - Exercicio sobre Personagem\r\n4 - Exercicio sobre Funcionarios\r\n5 - App Jokenpo");
             var opcao = Console.ReadLine();
             Console.WriteLine("------------------------------------");
             switch (opcao)
@@ -28,12 +31,10 @@ namespace Main
                     ProgramPersonagem.Main();
                     break;
                 case "4":
+                    ProgramFuncionarios.Main();
                     break;
                 case "5":
-                    break;
-                case "6":
-                    break;
-                case "7":
+                    ProgramJokenpo.Main();
                     break;
                 default:
                     break;
